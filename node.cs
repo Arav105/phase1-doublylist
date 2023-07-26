bf357b93af43bb3dcd4aceadc23f17b6e16ee1c1
Arav105 <chachuarav@gmail.com>
2023-07-26 20:50:52 +0530

    phase1-doublylist

---
+++ node.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace phase1_doublylist
+{
+    public class node
+    {
+        private string data;
+        private node next = null;
+        private node prev = null;
+
+        public string Data
+        {
+            get { return this.data; }
+            set { this.data = value; }
+        }
+
+        public node Next
+        {
+            get { return this.next; }
+            set { this.next = value; }
+        }
+
+        public node Prev
+        {
+            get { return this.prev; }
+            set { this.prev = value; }
+        }
+
+    }
+}
